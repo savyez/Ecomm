6fab7d6c290ab7391dc260c9e11a6cd073c51f35
Saurabh Biswas <visnuk252@gmail.com>
2024-01-09 23:45:05 +0530

    template configuration

--- Ecomm/views.py
+++ Ecomm/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1>This is Homepage.</h1>")
+    return render(request, 'index.html')
