4550e4690958e93314e49f87e2c2d88e93fb46c7
Saurabh Biswas <visnuk252@gmail.com>
2024-01-16 22:51:50 +0530

    Bug Fix: Unboundlocal error

--- cart/views.py
+++ cart/views.py
@@ -15,8 +15,8 @@ def add_cart(request, product_id):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
     except Cart.DoesNotExist:
-        Cart.objects.create(cart_id = _cart_id(request))
-    cart.save()
+        cart = Cart.objects.create(cart_id = _cart_id(request))
+        cart.save()
 
     
     try:
@@ -24,9 +24,8 @@ def add_cart(request, product_id):
         cart_item.quantity += 1
 
     except CartItem.DoesNotExist:
-        CartItem.objects.create(product=product, quantity=1, cart=cart)
-
-    cart_item.save()
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        cart_item.save()
     return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
