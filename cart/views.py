af417452795e2ef6ec8bbb00679175748b493123
Saurabh Biswas <visnuk252@gmail.com>
2024-01-18 13:13:40 +0530

    add cart functionality and banner

--- cart/views.py
+++ cart/views.py
@@ -67,3 +67,13 @@ def remove_cart_item(request, product_id):
     cart_item.delete()
     
     return redirect('cart')
+
+def add_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product= product, cart=cart)
+    
+    if(cart_item.quantity >= 1):
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('cart')
