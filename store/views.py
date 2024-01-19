92dc891af69c7954d2f833f6d3d09aa2783fde55
Saurabh Biswas <visnuk252@gmail.com>
2024-01-19 21:26:12 +0530

    Search functionality

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 from .models import Product
 from category.models import Category
@@ -41,3 +43,19 @@ def product_detail(request, category_slug, product_slug):
         'in_cart':in_cart
     }
     return render(request, "store/product_detail.html", context)
+
+
+# search functionality
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) |
+                                                                        Q(product_name__icontains=keyword))
+        product_count = products.count()
+        context = {
+        'products': products,
+        'product_count':product_count,
+        }
+        return render(request, 'store/store.html', context)
+    return render(request, 'store/store.html', context)
