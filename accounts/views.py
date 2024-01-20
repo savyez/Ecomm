6dda0db999ae26d2dfcb7bf9220515bc69642829
Saurabh Biswas <visnuk252@gmail.com>
2024-01-20 23:04:09 +0530

    registration form initialization

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'account/register.html', context)
+
+def login(request):
+    return HttpResponse('Login')
